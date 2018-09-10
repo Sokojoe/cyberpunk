7a7c8ac933a2ec739d33636b810b5e2815e41a87
Sokojoe <sokojoey@gmail.com>
2018-09-10 00:41:23 -0400

    Added parcel build

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const path = require('path')
 var app = express()
 
 const PORT = 8080
 
+app.use('../static', express.static(path.join(__dirname, 'static')))
+
 app.get('/', (req, res) => {
-  res.send('Welcome to Cyberpunk Roguelike!')
+ res.sendFile(path.join(__dirname, '../static/index.html'))
 })
 
 app.listen(PORT, () => {
