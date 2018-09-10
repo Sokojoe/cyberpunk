290fab865fd7c54bbb2bb16d36a897326e3ffaf1
Sokojoe <sokojoey@gmail.com>
2018-09-10 00:28:11 -0400

    Added linting

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var app = express()
 
 const PORT = 8080
 
-app.get('/', (req, res)=>{
-  res.send('Welcome to Cyberpunk Roguelike!');
-});
+app.get('/', (req, res) => {
+  res.send('Welcome to Cyberpunk Roguelike!')
+})
 
-app.listen(PORT, ()=>{
-  console.log(`Server started on port ${PORT}!`);
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}!`)
 })
