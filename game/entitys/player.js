9d20b2af762795472727a713f4ea7bf385508fb0
Sokojoe <SokoJoey@Gmail.com>
2018-10-01 00:38:07 -0400

    Add moveset to player

--- game/entitys/player.js
+++ game/entitys/player.js
@@ -4,6 +4,12 @@ class Player extends Entity {
   constructor (name, x, y) {
     super(name, 100, x, y)
     this.type = 'Player'
+    this.moveset = [
+      { x: 1, y: 0 },
+      { x: 0, y: 1 },
+      { x: -1, y: 0 },
+      { x: 0, y: -1 }
+    ]
   }
 }
 
