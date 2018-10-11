089a6f5ba8353f9b34738c6732f4d756e3158900
Sokojoe <SokoJoey@Gmail.com>
2018-10-11 01:11:06 -0400

    Lint test

--- test/test.js
+++ test/test.js
@@ -4,37 +4,37 @@ import Player from '../game/entitys/player'
 
 // Testing moveValidator
 test('moveValidator-isMoveValid-allowedMove', t => {
-  const player = new Player("Player", 0, 0)
-  const room = {height: 10, width:10}
+  const player = new Player('Player', 0, 0)
+  const room = { height: 10, width: 10 }
 
-  const moveValid = moveValidator.isMoveValid(player, room, {x: 0, y: 1})
+  const moveValid = moveValidator.isMoveValid(player, room, { x: 0, y: 1 })
 
   t.is(moveValid, true)
 })
 
 test('moveValidator-isMoveValid-moveNotInMoveSet', t => {
-  const player = new Player("Player", 0, 0)
-  const room = {height: 10, width:10}
+  const player = new Player('Player', 0, 0)
+  const room = { height: 10, width: 10 }
 
-  const moveValid = moveValidator.isMoveValid(player, room, {x: 2, y: 0})
+  const moveValid = moveValidator.isMoveValid(player, room, { x: 2, y: 0 })
 
   t.is(moveValid, false)
 })
 
 test('moveValidator-isMoveValid-moveOutOfBounds', t => {
-  const player = new Player("Player", 0, 0)
-  const room = {height: 10, width:10}
+  const player = new Player('Player', 0, 0)
+  const room = { height: 10, width: 10 }
 
-  const moveValid = moveValidator.isMoveValid(player, room, {x: -1, y: 0})
+  const moveValid = moveValidator.isMoveValid(player, room, { x: -1, y: 0 })
 
   t.is(moveValid, false)
 })
 
 test('moveValidator-calculateMoveSet', t => {
-  const player = new Player("Player", 9, 9)
-  const room = {height: 10, width:10}
+  const player = new Player('Player', 9, 9)
+  const room = { height: 10, width: 10 }
 
   const moveSet = moveValidator.calculateMoveSet(player, room)
 
-  t.deepEqual(moveSet, [{x: -1, y: 0}, {x: 0, y: -1}])
+  t.deepEqual(moveSet, [{ x: -1, y: 0 }, { x: 0, y: -1 }])
 })
