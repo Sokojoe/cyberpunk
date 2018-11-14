557ddaff106d645b33b0689b3f2331aa96811680
Sokojoe <sokojoey@gmail.com>
2018-11-14 02:39:07 -0500

    Implemented second half of collisions. Collisions are now handled.

--- game/logic/turn-engine.js
+++ game/logic/turn-engine.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Coordinate = require('../tiles/coordinate')
+const moveValidator = require('../validators/moveValidator')
 
 function processMoves (room, entities, entitiesMove) {
   const newEntities = {}
@@ -8,11 +9,13 @@ function processMoves (room, entities, entitiesMove) {
   for (const entityId in entities) {
     const move = entitiesMove[entityId]
     let uniquePosition = true
+    let collisionPosition
     // if the entity is moving this turn
     if (move) {
       // check that the entities desired position isn't already taken
       for (const e in newEntities) {
         if (Coordinate.isSame(move, newEntities[e].position)) {
+          collisionPosition = newEntities[e].position
           uniquePosition = false
           break
         }
@@ -24,15 +27,43 @@ function processMoves (room, entities, entitiesMove) {
         newEntities[entityId].position = entitiesMove[entityId]
       } else {
         // Else find the second closest square to the entity which is free
-        // TODO: fix this
+        const closestCoordinate = findClosestCoordinate(room, entities[entityId].position, collisionPosition, newEntities)
+        
         newEntities[entityId] = entities[entityId]
-        newEntities[entityId].position = new Coordinate(1, 1)
+        newEntities[entityId].position = closestCoordinate
       }
     }
   }
 
   // re-assign the entities list
   entities = newEntities
+}
+
+function findClosestCoordinate (room, currentPosition, desiredPosition) {
+  // Note, its possible none of these squares are available. Possibly might have to go for an interative approach
+  const moveSet = [
+    new Coordinate(0, 1),
+    new Coordinate(0, -1),
+    new Coordinate(1, 0),
+    new Coordinate(-1, 0)
+  ]
+
+  // Mock entity to find valid squares surrounding desired position
+  const entity = { position: desiredPosition, moveSet: moveSet }
+  const validMoveSet = moveValidator.calculateMoveSet(entity, room)
+  console.log(validMoveSet)
+  let bestMove
+  let shortestDist = 10000
+  for (const key in validMoveSet) {
+    const potentialMove = validMoveSet[key]
+    const potentialMoveCoords = new Coordinate(potentialMove.x + desiredPosition.x, potentialMove.y + desiredPosition.y)
+    const dist = Math.sqrt(Math.pow((potentialMoveCoords.x - currentPosition.x), 2) + Math.pow((potentialMoveCoords.y - currentPosition.y), 2))
+    if (dist < shortestDist) {
+      shortestDist = dist
+      bestMove = potentialMoveCoords
+    }
+  }
+  return bestMove
 }
 
 module.exports = processMoves
